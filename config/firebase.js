9f594a55772229c5b243276fd15b5d46dcb195f8
Shantam Verma <contactshantam@gmail.com>
2024-02-07 00:04:01 +0530

    minor bug

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { FIREBASE_API_KEY } from "@/utils/constants";
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,6 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics =
+  app.name && typeof window !== "undefined" ? getAnalytics(app) : null;
 const auth = getAuth();
 export { app, auth };
